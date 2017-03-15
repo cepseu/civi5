4debda3719e722d609fac41f3f4fb0f6260edd7d
Domi <dominique@dominiquedecooman.com>
2017-03-15 18:03:13 +0100

    only cc all in update script for dev

--- bash/updates/update_dev.sh
+++ bash/updates/update_dev.sh
@@ -5,45 +5,45 @@ cd /var/www/customprojects/ceps/dev/docroot
 #D7
 
 #Update site
-drush updb -y
+#drush updb -y
 
 #Enable ui modules
-drush en features fe_block ftools features_orphans diff -y
-drush en field_ui update schema devel feeds_ui views_ui switcheroo dblog glue simpletest masquerade -y
+#drush en features fe_block ftools features_orphans diff -y
+#drush en field_ui update schema devel feeds_ui views_ui switcheroo dblog glue simpletest masquerade -y
 
 # Clear caches
 drush cc all
 
 #Feature reverts
-drush fra -y
+#drush fra -y
 
 #Enable the views development settings
-drush php-eval 'views_development_settings()'
+#drush php-eval 'views_development_settings()'
 
 #Rebuild content permissions
-drush php-eval 'node_access_rebuild()'
+#drush php-eval 'node_access_rebuild()'
 
 # Clear caches
-drush cc all
+#drush cc all
 
 #Reindex content Optional
 #drush search-reindex -y
 #drush search-index -y
 #OR
 #Solr
-SOLRACTIVE=`cat ../config/config | grep '^solractive:' | sed 's/^solractive://g'`
-echo 'solractive:'$SOLRACTIVE
-if [ -n "$SOLRACTIVE" ]; then
+#SOLRACTIVE=`cat ../config/config | grep '^solractive:' | sed 's/^solractive://g'`
+#echo 'solractive:'$SOLRACTIVE
+#if [ -n "$SOLRACTIVE" ]; then
   #set the solr env
-  drush php-eval 'glue_site_set_solr_settings('apache_solr')'
+#  drush php-eval 'glue_site_set_solr_settings('apache_solr')'
   #index
-  drush search-api-clear apache_solr_index
-  drush sapi-aq
-  drush queue-run-concurrent search_api_indexing_queue 8
-fi
+#  drush search-api-clear apache_solr_index
+#  drush sapi-aq
+#  drush queue-run-concurrent search_api_indexing_queue 8
+#fi
 
 # Run cron
-drush cron
+#drush cron
 
 #Set dev settings
 drush vset --yes --always-set preprocess_css 0
