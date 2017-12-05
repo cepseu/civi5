e6117a179e9c6fdc09444c122b3c18124a23dfb8
root <root@cronus.c.hosting-165709.internal>
2017-12-05 17:41:08 +0100

    Restore alias symlink to system drush directory

--- etc/drush/ceps.aliases.drushrc.php
+++ etc/drush/ceps.aliases.drushrc.php
@@ -13,23 +13,14 @@
     $ip_production = '92.243.15.236';
     $remote_user_production = 'admin';
 
-    if (file_exists('/etc/drush/' . $project . '.local.php')) {
-      require '/etc/drush/' . $project . '.local.php';
-    }
-
     $aliases[$project.'.dev'] = array(
       'root' => '/var/www/customprojects/ceps/dev/docroot',
-      'remote-host' => $ip_dev,
-      'remote-user' => $remote_user_dev,
       'path-aliases' => array(
         '%dump' => '/tmp/sql-sync-dev-'.$project.'-local.sql',
       )
     );
-
     $aliases[$project.'.staging'] = array(
       'root' => '/var/www/customprojects/ceps/staging/docroot',
-      'remote-host' => $ip_staging,
-      'remote-user' => $remote_user_staging,
       'path-aliases' => array(
         '%dump' => '/tmp/sql-sync-staging-'.$project.'-local.sql',
       )
@@ -37,18 +28,13 @@
 
     $aliases[$project.'.testbot1'] = array(
       'root' => '/var/www/customprojects/ceps/testbot1/docroot',
-      'remote-host' => $ip_testbot1,
-      'remote-user' => $remote_user_testbot1,
       'path-aliases' => array(
         '%dump' => '/tmp/sql-sync-testbot1-'.$project.'-local.sql',
       )
 
     );
-
     $aliases[$project.'.production'] = array(
       'root' => '/var/www/customprojects/ceps/production/docroot',
-      'remote-host' => $ip_production,
-      'remote-user' => $remote_user_production,
       'path-aliases' => array(
         '%dump' => '/tmp/sql-sync-production-'.$project.'-local.sql',
       )
